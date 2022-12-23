90a04ad2b36e2b5018188cadfa3c4d7572a60750
firchun025 <firchun025@gmail.com>
2022-12-23 19:36:32 +0900

    add chapter content

---
+++ app/src/main/java/com/example/mathkomik/chapter1.java
@@ -0,0 +1,25 @@
+package com.example.mathkomik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
+
+
+public class chapter1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter1);
+
+    }
+
+
+
+    }
--- app/src/main/java/com/example/mathkomik/daftar.java
+++ app/src/main/java/com/example/mathkomik/daftar.java
@@ -7,14 +7,54 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.mathkomik.databinding.ActivityChapter1Binding;
+
 public class daftar extends AppCompatActivity {
 
-
+    Button btn_chapter1;
+    Button btn_chapter2;
+    Button btn_chapter3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
+
+        //tombol chapter 1
+
+        btn_chapter1 = (Button) findViewById(R.id.chapter1);
+
+        btn_chapter1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent beach = new  Intent(daftar.this, chapter1.class);
+                startActivity(beach);
+            }
+        });
+
+        //tombol chapter 2
+
+        btn_chapter2 = (Button) findViewById(R.id.chapter2);
+
+        btn_chapter2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent beach = new  Intent(daftar.this, chapter2.class);
+                startActivity(beach);
+            }
+        });
+
+        //tombol chapter 3
+
+        btn_chapter3 = (Button) findViewById(R.id.chapter3);
+
+        btn_chapter3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent beach = new  Intent(daftar.this, chapter3.class);
+                startActivity(beach);
+            }
+        });
 
 
     }
