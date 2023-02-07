dafed6b5b1c5ecbcf6e94fb467fe544b89f4cd94
firchun025 <firchun025@gmail.com>
2023-02-07 23:52:21 +0900

    add chapter and pengenalan tokoh

--- app/src/main/java/com/example/mathkomik/daftar.java
+++ app/src/main/java/com/example/mathkomik/daftar.java
@@ -14,6 +14,8 @@ public class daftar extends AppCompatActivity {
     Button btn_chapter1;
     Button btn_chapter2;
     Button btn_chapter3;
+    Button btn_chapter4;
+    Button btn_chapter5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +57,31 @@ public class daftar extends AppCompatActivity {
                 startActivity(beach);
             }
         });
+
+        //tombol chapter 4
+
+        btn_chapter4 = (Button) findViewById(R.id.chapter4);
+
+        btn_chapter4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent beach = new  Intent(daftar.this, chapter4.class);
+                startActivity(beach);
+            }
+        });
+
+        //tombol chapter 5
+
+        btn_chapter5 = (Button) findViewById(R.id.chapter5);
+
+        btn_chapter5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent beach = new  Intent(daftar.this, chapter5.class);
+                startActivity(beach);
+            }
+        });
+
 
 
     }
